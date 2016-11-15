0f3fc5aafc1f584431d0a7546b2c4230f1754c48
admin <admin@iMac-de-admin-3.local>
2016-11-15 13:01:29 +0100

    Step4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
+import {AuthHttp, AuthConfig} from 'angular2-jwt';
+import {Http} from '@angular/http';
+import { Storage } from '@ionic/storage';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HeaderContentComponent } from '../components/header-content/header-content';
 import { Routes } from './app.routes';
+
+let storage = new Storage();
+
+
+export function getAuthHttp(http) {
+  return new AuthHttp(new AuthConfig({
+    noJwtError: true,
+    globalHeaders: [{'Accept': 'application/json'}],
+    tokenGetter: (() => storage.get('id_token')),
+  }), http);
+}
 
 const app:Array<any>=[MyApp];
 const pages:Array<any> = Routes.getPages();
@@ -25,5 +39,12 @@ const appIonicConfig = {
   ],
   bootstrap: [IonicApp],
   entryComponents: app.concat(pages),
+  providers: [
+    {
+      provide: AuthHttp,
+      useFactory: getAuthHttp,
+      deps: [Http]
+    },
+  ]
 })
 export class AppModule {}
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
 import { Routes } from '../../app/app.routes';
 
 /*
@@ -14,10 +14,23 @@ import { Routes } from '../../app/app.routes';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {}
 
   goTabs(){
     this.navCtrl.push(Routes.getPage(Routes.TABS));
   }
 
+  login() : void {
+    let loginPage = Routes.getPage(Routes.LOGIN);
+    //this.navCtrl.push(loginPage);
+    let loginModal = this.modalCtrl.create(loginPage);
+    loginModal.present();
+
+  }
+
+  signUp() : void {
+    let signUpPage = Routes.getPage(Routes.SIGNUP);
+    let signUpModal = this.modalCtrl.create(signUpPage);
+    signUpModal.present();
+  }
 }
